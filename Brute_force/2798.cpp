1b1701d37f617a1d101337120ece787de49e97b9
YEJIN-LILY <57944153+YEJIN-LILY@users.noreply.github.com>
2020-08-17 22:48:06 +0900

    Correct: 2798

--- Brute_force/2798.cpp
+++ Brute_force/2798.cpp
@@ -12,10 +12,10 @@ int main(){
 	
 	int result=0;
 	
-	for(int i=0;i<n-2;i++){
+	for(int i=0;i<n-2;i++){ //세 수 선택
 		for(int j=i+1;j<n-1;j++){
 			for(int k=j+1;k<n;k++){
-				if(nums[i]+nums[j]+nums[k]<=m&&m-(nums[i]+nums[j]+nums[k])<=m-result)
+				if(nums[i]+nums[j]+nums[k]<=m&&m-(nums[i]+nums[j]+nums[k])<=m-result) //세 수의 합이 m이하이고 차이가 이전에 계산한 값보다 작으면
 					result=nums[i]+nums[j]+nums[k];
 			}
 		}
