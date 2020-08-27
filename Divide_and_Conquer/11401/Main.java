40538021662771a0e2f2419d7f7dd6c347182bee
HSHyeon <60344240+HSHyeon@users.noreply.github.com>
2020-08-27 15:59:00 +0900

    Update Main.java

--- Divide_and_Conquer/11401/Main.java
+++ Divide_and_Conquer/11401/Main.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
+//11401 이항계수3
+
+class  Main 
+{
+static int[] arr;
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		long div=1000000007;
+
+		int N=sc.nextInt();
+		int S=sc.nextInt();
+		
+		arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+				arr[i] = sc.nextInt();
+		}
+		
+		Solve();
+		int result=num%div;
+
+		System.out.println(result);
+		}
+
+	public static long fac(int num){
+		
+	}
+
+	public static int Solve(){
+		fac(N)//nCr=nCk+n-1Cr
+
+	}	
+}
 
